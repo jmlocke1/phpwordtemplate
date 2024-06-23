6197e91ba78e3da9642b3dc3e79af097cf21c3cb
Josemi <worldcode.info@gmail.com>
2024-06-23 21:23:41 +0200

    Añadidos los ejemplos del repositorio

--- inicio.php
+++ inicio.php
@@ -1,7 +1,7 @@
 <?php
 // Activamos el autoloader de Composer
 require __DIR__.'/vendor/autoload.php';
-require_once 'bootstrap.php';
+// require_once 'bootstrap.php';
 
 use PhpOffice\PhpWord\IOFactory;
 use PhpOffice\PhpWord\PhpWord;
@@ -23,8 +23,8 @@ $seccion->addText(
 
 
 // Guardando documento
-$objWriter = IOFactory::createWriter($documento, "Word2022");
-$objWriter->save("Documento01.docx");
+// $objWriter = IOFactory::createWriter($documento, "Word2007");
+$documento->save("Documento01.docx", "Word2007");
 
-header("Content-Disposition: attachment; filename='Documento01.docx'");
+header("Content-Disposition: attachment; filename=Documento01.docx");
 echo file_get_contents('Documento01.docx');
