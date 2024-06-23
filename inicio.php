492fdc566d5d1b828d1eecdecb50c96251d5eef8
Josemi <worldcode.info@gmail.com>
2024-06-23 19:40:22 +0200

    Commit inicial. Instalación phpword con composer e inicio del proyecto

---
+++ inicio.php
@@ -0,0 +1,30 @@
+<?php
+// Activamos el autoloader de Composer
+require __DIR__.'/vendor/autoload.php';
+require_once 'bootstrap.php';
+
+use PhpOffice\PhpWord\IOFactory;
+use PhpOffice\PhpWord\PhpWord;
+use PhpOffice\PhpWord\Style\Font;
+
+// Instancia phpWord
+// $phpWord = new PhpWord();
+$documento = new PhpWord();
+
+// Nueva sección
+$seccion = $documento->addSection();
+
+// Texto sin formato
+$seccion->addText(
+    htmlspecialchars(
+        'Primer texto - Texto sin formato'
+    )
+);
+
+
+// Guardando documento
+$objWriter = IOFactory::createWriter($documento, "Word2022");
+$objWriter->save("Documento01.docx");
+
+header("Content-Disposition: attachment; filename='Documento01.docx'");
+echo file_get_contents('Documento01.docx');
